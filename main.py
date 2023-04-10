1b31c73e39e196d9bf288b733cde55086c9921ec
medhidev <grimalmedhi4@gmail.com>
2023-04-10 13:17:48 +0200

    case noir pour la moitié gauche de la matrice (select)

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ def creeCube(n, m):
     matrice = [[ random.randint(0, 1) for x in range(m)] for y in range(n)]
     return matrice
 
-def printCube(matrice):
-    n = len(matrice)
-    m = len(matrice[0])
+# Pour les test
+# def printCube(matrice):
+#     n = len(matrice)
+#     m = len(matrice[0])
 
-    for i in range(n):
-        for j in range(m):
-            print(matrice[i][j],end=" ")
+#     for i in range(n):
+#         for j in range(m):
+#             print(matrice[i][j],end=" ")
 
-        #saut de ligne
-        print(" ")
+#         #saut de ligne
+#         print(" ")
 
 def coloration(matrice):
     n = len(matrice)
@@ -25,16 +26,27 @@ def coloration(matrice):
 
     for i in range(n):
         for j in range(m):
-            if matrice[i][j] == 1:
-                print('\u25a0', end=" ")
-            else:
+            if (j < m//2):
                 print('\u25a1', end=" ")
+            else:
+                if matrice[i][j] == 1:
+                    # ■
+                    print('\u25a0', end=" ")
+                else:
+                    # □
+                    print('\u25a1', end=" ")
+
         print("")
 
 
-# def symetrieCube():
-#     # Système de symétrie
-#     return 
+def symetrieCube(matrice):
+    n = len(matrice)
+    midsize = len(matrice[0])//2
+
+    for i in range(n):
+        for j in range(midsize):
+            print('\u25a1', end=" ")
+        print("")
 
 
 def main():
@@ -48,7 +60,7 @@ def main():
     size = ''
 
     while(size == ''):
-        size = input('dimension du cube (avec Longeur;largeur): ')
+        size = input('dimension du cube (avec L;l paire): ')
         for i in (size):
             if (i == ';'):
                 changeur += 1
@@ -57,8 +69,19 @@ def main():
             else:
                 longueur += i
 
+        # if(int(longueur + largeur)%2 != 0):
+        #     print('Erreur, les dimension ne sont pas paire')
+        #     size = ''
+
+        # if(int(longueur) != int(largeur)):
+        #     size = ''
+        #     print('Erreur, les dimensions doivent être les mêmes')
+        
+        # print(f'L={longueur}; l={largeur}')
+
     cube = creeCube(int(longueur), int(largeur))
     # printCube(cube)
     coloration(cube)
+    # symetrieCube(cube)
 
 main()
